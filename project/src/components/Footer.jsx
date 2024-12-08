bd34093137ba2fb69265ab3bba71aa576339a7db
Nuru <nuruzzamannuru7595@gmail.com>
2024-12-08 08:13:46 +0600

    fix

--- project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <div className="title myBorder">
-                <p className="py-1 text-lg">Copyright &copy; 2024 Nuru</p>
-            </div>
-        </footer>
-    );
-}
